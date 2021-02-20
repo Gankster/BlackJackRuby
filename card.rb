a75732d420f08b0425548c7e0681d7a59cbda258
Andrii Boreiko <andriyboreyko@gmail.com>
2021-02-20 19:48:04 +0200

    review

--- card.rb
+++ card.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
 class Card
+  SUITS = {
+    spade: "\u2660",
+    heart: "\u2665",
+    diamond: "\u2666",
+    club: "\u2663"
+  }.freeze
+
+  CARDS = {
+    '2' => [2],
+    '3' => [3],
+    '4' => [4],
+    '5' => [5],
+    '6' => [6],
+    '7' => [7],
+    '8' => [8],
+    '9' => [9],
+    '10' => [10],
+    'B' => [10],
+    'Q' => [10],
+    'K' => [10],
+    'A' => [1, 11]
+  }.freeze
+
   attr_reader :char, :suit, :value, :name
 
   def initialize(char, suit, value)
--- deck.rb
+++ deck.rb
@@ -1,29 +1,6 @@
 # frozen_string_literal: true
 
 class Deck
-  SUITS = {
-    spade: "\u2660",
-    heart: "\u2665",
-    diamond: "\u2666",
-    club: "\u2663"
-  }.freeze
-
-  CARDS = {
-    '2' => [2],
-    '3' => [3],
-    '4' => [4],
-    '5' => [5],
-    '6' => [6],
-    '7' => [7],
-    '8' => [8],
-    '9' => [9],
-    '10' => [10],
-    'B' => [10],
-    'Q' => [10],
-    'K' => [10],
-    'A' => [1, 11]
-  }.freeze
-
   attr_reader :cards
 
   def initialize
@@ -34,8 +11,8 @@ class Deck
 
   def create_cards
     cards = []
-    CARDS.each do |char, value|
-      SUITS.each_value do |suit|
+    Card::CARDS.each do |char, value|
+      Card::SUITS.each_value do |suit|
         cards << Card.new(char, suit, value)
       end
     end
